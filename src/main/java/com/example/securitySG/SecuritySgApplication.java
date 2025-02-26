6edf9f9f0344113d3f71d8509648b30ee3a6400c
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-02-25 21:19:04 -0300

    chore: iniciando projeto a partir do spring initializr com dependencias spring web e security

---
+++ src/main/java/com/example/securitySG/SecuritySgApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitySG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securitySG/SecuritySgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitySG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
