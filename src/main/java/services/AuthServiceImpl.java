4c03b591540f0cde09906e238072c6135e52e9c2
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-07 11:58:59 -0300

    feat: Implementação do serviço de autenticação - Implementados métodos para autenticação de usuários e geração de tokens JWT.

---
+++ src/main/java/services/AuthServiceImpl.java
@@ -0,0 +1,67 @@
+package services;
+
+import dtos.LoginDto;
+import infra.config.JwtTokenProvider;
+import models.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import repostories.UserRepository;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class AuthServiceImpl {
+
+    private JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+    private PasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public AuthServiceImpl(JwtTokenProvider jwtTokenProvider, UserRepository userRepository, PasswordEncoder bCryptPasswordEncoder) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public Map<String, String> login(LoginDto loginDto) {
+        Optional<UserEntity> userOptional = userRepository.findByUsername(loginDto.getUsername());
+
+        // Validação de entrada
+        if(userOptional.isPresent()) {
+            UserEntity user = userOptional.get();
+
+            if (bCryptPasswordEncoder.matches(loginDto.getPassword(), user.getPassword())) {
+
+                //retorna objeto mensagem
+                return Map.of("mensagem:", "usuario logado");
+            }
+            return Map.of("mensagem:", "senha invalida");
+        }
+        return Map.of("mensagem:", "userName invalido");
+    }
+
+    private String authenticateUser() {
+        // Obtém o objeto Authentication do contexto de segurança
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Gera o token JWT
+        return jwtTokenProvider.generateToken(authentication);
+    }
+
+    public String refreshToken(String refreshToken) {
+        // Implementação de refresh token (aqui apenas retorna o mesmo token como exemplo)
+        return refreshToken;
+    }
+
+    public ResponseEntity<?> login(ResponseEntity<?> login) {
+        // Exemplo de metodo ResponseEntity
+        return login;
+    }
+
+
+}
