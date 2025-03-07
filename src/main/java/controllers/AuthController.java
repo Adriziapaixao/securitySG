bc504b1e4283d0cd93d3655e839608d4899c9258
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-07 11:58:05 -0300

    feat: Implementação do controlador de autenticação - Adicionado endpoint para login e geração de tokens JWT. - Adicionado endpoint para refresh token.

---
+++ src/main/java/controllers/AuthController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import services.AuthService;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<String> refreshToken(@RequestHeader("refresh-token") String refreshToken){
+        String responseDto = authService.refreshToken(refreshToken);
+
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+
+    // Tratamento de exceções de validação
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
+    }
+}
