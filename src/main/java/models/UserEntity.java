9f3b3a848d34e9197850052d5983bb4475ac0818
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-07 12:03:14 -0300

    feat: Entidade de usuário - Adicionada entidade para persistência de dados de usuários no banco de dados.

---
+++ src/main/java/models/UserEntity.java
@@ -0,0 +1,27 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+}
+
+
+
