3f8216df1e4051cfd81cdf9c5de934815ebc554e
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-07 12:00:44 -0300

    feat: Entidade de roles (permissões) - Adicionada entidade para representar os papéis de usuário no sistema.

---
+++ src/main/java/models/RoleEntity.java
@@ -0,0 +1,25 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public void RoleEntity(String name) {
+        this.name = name;
+    }
+    public void RoleEntity(){
+
+    }
+}
+
