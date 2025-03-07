c000a689fc58603936a9efc6e0a365d93aa3f233
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-07 12:00:28 -0300

    feat: Entidade de refresh token - Adicionada entidade para persistência de tokens de atualização no banco de dados.

---
+++ src/main/java/models/RefreshTokenEntity.java
@@ -0,0 +1,33 @@
+package models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@Entity
+@Data
+public class RefreshTokenEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+}
